84e4d77b857285d510f2b43592b26ac241023e85
Cleverson <smileyznl@gmail.com>
2023-02-24 01:15:14 -0300

    Updated Hash.h
    Updated Hash.h

--- MatchBot/Hash.h
+++ MatchBot/Hash.h
@@ -7,25 +7,25 @@ namespace Hash
 {
 	namespace internal
 	{
-		constexpr std::uint32_t Basis{ 0x811c9dc5 };
-		constexpr std::uint32_t Prime{ 0x1000193 };
+		constexpr unsigned int Basis{ 0x811c9dc5 };
+		constexpr unsigned int Prime{ 0x1000193 };
 	}
 
-	static inline constexpr std::uint32_t GetConst( const char* string, const std::uint32_t value = internal::Basis ) noexcept
+	static inline constexpr unsigned int GetConst(const char* string, const unsigned int value = internal::Basis) noexcept
 	{
-		return ( string[ 0 ] == '\0' ) ? value : Hash::GetConst( &string[ 1 ], ( value ^ static_cast< std::uint32_t >( string[ 0 ] ) ) * internal::Prime );
+		return (string[ 0 ] == '\0') ? value : GetConst(&string[1], (value ^ static_cast<unsigned int>(string[0])) * internal::Prime);
 	}
 
-	static inline std::uint32_t Get( const std::string_view& string )
+	static inline unsigned int Get(const std::string_view& string)
 	{
-		auto ret{ internal::Basis };
+		auto result { internal::Basis };
 
 		for ( auto i{ 0u }; i < string.size( ); ++i )
 		{
-			ret ^= string[ i ];
-			ret *= internal::Prime;
+			result ^= string[ i ];
+			result *= internal::Prime;
 		}
 
-		return ret;
+		return result;
 	}
 }
