6fe3f65a38b29ca2d52dc99f6b290d617f8f4b52
Cleverson <smileyznl@gmail.com>
2023-02-24 00:55:22 -0300

    Fixed Hash.h
    Fixed Hash.h

--- MatchBot/MatchCommand.cpp
+++ MatchBot/MatchCommand.cpp
@@ -3,7 +3,7 @@
 CMatchCommand gMatchCommand;
 
 // Match BOT Commands Hash
-enum MatchCommandConst : std::uint32_t
+enum MatchCommandConst : unsigned int
 {
 	// Basic Stuff
 	Command_Say = Hash::GetConst("say"),
