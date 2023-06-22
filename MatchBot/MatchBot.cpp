3612adbbd162b72d7a6f6aee9c933c9fdaf60108
Cleverson <smileyznl@gmail.com>
2023-06-22 10:26:11 -0300

    Adjust default mb_fix_spawn_distance value

--- MatchBot/MatchBot.cpp
+++ MatchBot/MatchBot.cpp
@@ -102,7 +102,7 @@ void CMatchBot::ServerActivate()
 	this->m_ExtraSmokeCount = gMatchUtil.CvarRegister("mb_extra_smoke_count", "2");
 
 	// Distance to respawn player if try to bug respawn on de_dust2 ramp and other maps (Distance in units to enable fix, 0 to disable)
-	this->m_FixSpawnDistance = gMatchUtil.CvarRegister("mb_fix_spawn_distance", "25.0");
+	this->m_FixSpawnDistance = gMatchUtil.CvarRegister("mb_fix_spawn_distance", "60.0");
 
 	// Users Help File or Website url (Without HTTPS)
 	this->m_HelpFile = gMatchUtil.CvarRegister("mb_help_file", "cstrike/addons/matchbot/users_help.html");
