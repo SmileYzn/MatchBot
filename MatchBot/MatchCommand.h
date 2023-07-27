ddd1113e740303bcabc7ad95b750d4f01d0eb3db
yuyi <yuyi@PC>
2023-07-27 16:53:01 +0200

    Rename mbCommand to pCommand

--- MatchBot/MatchCommand.h
+++ MatchBot/MatchCommand.h
@@ -6,7 +6,7 @@ public:
 	// On Client Command
 	bool ClientCommand(CBasePlayer* Player, const char* pcmd, const char* parg1);
 	// Prefix command comparison
-	bool CompareCommand(const char* pcmd, const char* prefix, const char* mbCommand);
+	bool CompareCommand(const char* pcmd, const char* prefix, const char* pCommand);
 };
 
 extern CMatchCommand gMatchCommand;
