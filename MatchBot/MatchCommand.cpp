be8c010de17ff82eb8ed4b9307121a858ed1b1a6
Cleverson <smileyznl@gmail.com>
2023-12-19 13:28:35 -0300

    Removed debug

--- MatchBot/MatchCommand.cpp
+++ MatchBot/MatchCommand.cpp
@@ -35,9 +35,6 @@ void CMatchCommand::ServerActivate()
 					// Command Flag
 					auto Flag = gMatchAdmin.ReadFlags(lpMemScript->GetAsString().c_str());
 
-					// Debug
-					LOG_CONSOLE(PLID, "[%s] %d %s %d", __func__, Index, Command.c_str(), Flag);
-
 					// Insert to container
 					this->m_Data[Command] = {Index, Flag};
 				}
